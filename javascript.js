e3fb004daa0c4af7c7091abc3ef07ef83500d1df
Ernest <143043338+e05910879@users.noreply.github.com>
2023-09-15 16:46:01 -0700

    Set style attributes for squares in javascript

--- javascript.js
+++ javascript.js
@@ -8,9 +8,12 @@ body.insertBefore(grid, script);
 
 // Fill grid with squares.
 const SIZE = 16;
+const WIDTH = 960;
+const SQUARE_WIDTH = Math.floor(WIDTH / SIZE);
 for (let i = 0; i < SIZE * SIZE; i++) {
     const square = document.createElement('div');
     square.setAttribute('class', 'square');
+    square.setAttribute('style', `width: ${SQUARE_WIDTH}px; height: ${SQUARE_WIDTH}px; background-color: white;`);
     grid.appendChild(square);
 }
 
