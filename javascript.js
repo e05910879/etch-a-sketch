b1959c054e14e08f3a9e4c275970bb549f84f289
Ernest <143043338+e05910879@users.noreply.github.com>
2023-09-18 17:29:16 -0700

    Reorganize code into functions

--- javascript.js
+++ javascript.js
@@ -6,31 +6,38 @@ const container = document.createElement('div');
 container.setAttribute('id', 'container');
 body.insertBefore(container, script);
 
-// Create grid.
-const WIDTH = 720;
-const grid = document.createElement('div');
-grid.setAttribute('id', 'grid');
-grid.setAttribute('style', `width: ${WIDTH}px; height: ${WIDTH}px;`)
-container.appendChild(grid);
-
-// Fill grid with squares.
-const SIZE = 16;
-const SQUARE_WIDTH = Math.floor(WIDTH / SIZE);
-for (let i = 0; i < SIZE * SIZE; i++) {
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    square.setAttribute('style', `width: ${SQUARE_WIDTH}px; height: ${SQUARE_WIDTH}px;`);
-    grid.appendChild(square);
+function createPerimeter(width) {
+    const perimeter = document.createElement('div');
+    perimeter.setAttribute('id', 'perimeter');
+    perimeter.setAttribute('style', `width: ${width}px; height: ${width}px;`)
+    container.appendChild(perimeter);
 }
 
-// Hover
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-        square.classList.add('hover');
-    });
-    square.addEventListener('mouseleave', () => {
-        square.classList.remove('hover');
-    });
-});
+function createSquares(width, numSquares) {
+    const SQUARE_WIDTH = Math.floor(width / numSquares);
+    for (let i = 0; i < numSquares * numSquares; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.setAttribute('style', `width: ${SQUARE_WIDTH}px; height: ${SQUARE_WIDTH}px;`);
+        perimeter.appendChild(square);
+    }
+}
 
+function addHoverEffect() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('hover');
+        });
+        square.addEventListener('mouseleave', () => {
+            square.classList.remove('hover');
+        });
+    });
+}
+
+// Create grid.
+function createGrid(width, numSquares) {
+    createPerimeter(width);
+    createSquares(width, numSquares);
+    addHoverEffect();
+}
